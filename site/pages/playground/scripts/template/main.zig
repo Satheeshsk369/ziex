e6944f1860be76dc32647417fd74f7b564a99b97
Nurul Huda (Apon) <me@nurulhudaapon.com>
2026-03-05 13:25:29 +0600

    docs(pg): add support for componentctx

--- site/pages/playground/scripts/template/main.zig
+++ site/pages/playground/scripts/template/main.zig
@@ -7,16 +7,33 @@ pub fn main() !void {
     var aw = std.Io.Writer.Allocating.init(allocator);
 
     if (!@hasDecl(pg, "Playground")) {
-        @compileError("`pub fn Playground(allocator: zx.Allocator) zx.Component` is required");
+        @compileError("`pub fn Playground` is required");
     }
 
-    const pg_type = @TypeOf(pg.Playground);
-    const expected_type = fn (allocator: zx.Allocator) zx.Component;
-
-    if (pg_type != expected_type) {
-        @compileError("`Playground` type needs to be `fn (allocator: zx.Allocator) zx.Component`");
-    }
-
-    try pg.Playground(allocator).render(&aw.writer);
+    const component = resolveComponent(allocator);
+    try component.render(&aw.writer);
     try std.fs.File.stdout().writeAll(aw.written());
+}
+
+fn resolveComponent(allocator: zx.Allocator) zx.Component {
+    const FnInfo = @typeInfo(@TypeOf(pg.Playground)).@"fn";
+    const param_count = FnInfo.params.len;
+    const FirstParam = FnInfo.params[0].type.?;
+
+    // fn(ctx: *zx.ComponentContext) zx.Component
+    if (param_count == 1 and @typeInfo(FirstParam) == .pointer and
+        @hasField(@typeInfo(FirstParam).pointer.child, "allocator") and
+        @hasField(@typeInfo(FirstParam).pointer.child, "children"))
+    {
+        const ctx = allocator.create(@typeInfo(FirstParam).pointer.child) catch @panic("OOM");
+        ctx.* = .{ .allocator = allocator, .props = {} };
+        return pg.Playground(ctx);
+    }
+
+    // fn(allocator: zx.Allocator) zx.Component
+    if (param_count == 1 and FirstParam == zx.Allocator) {
+        return pg.Playground(allocator);
+    }
+
+    @compileError("`Playground` must be `fn (*zx.ComponentContext) zx.Component` or `fn (zx.Allocator) zx.Component`");
 }
